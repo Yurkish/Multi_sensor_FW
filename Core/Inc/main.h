c22e4293cebe62f157b6c6071480e5f69a92b2ac
Yurkish <rassadinj@gmail.com>
2021-10-20 16:41:26 +0300

    Testing comment added
    I'm testing this GUI

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -13,7 +13,7 @@
   *
   * Copyright (c) 2018 STMicroelectronics International N.V. 
   * All rights reserved.
-  *
+  * fgfghfgjghjhgjhgj
   * Redistribution and use in source and binary forms, with or without 
   * modification, are permitted, provided that the following conditions are met:
   *
