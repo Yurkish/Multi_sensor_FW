d9c291295400bbf71cce40814534bf489c005fe6
Yurkish <rassadinj@gmail.com>
2021-10-21 13:18:11 +0300

    include main.h
    Defined GPIO

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -89,7 +89,16 @@ void Error_Handler(void);
 
 /* Private defines -----------------------------------------------------------*/
 /* USER CODE BEGIN Private defines */
-
+/* USER CODE BEGIN Private defines */
+#define measure_Pin GPIO_PIN_3
+#define measure_GPIO_Port GPIOA
+#define term_vcc_Pin GPIO_PIN_8
+#define term_vcc_GPIO_Port GPIOB
+#define RF_CTRL1_Pin GPIO_PIN_13
+#define RF_CTRL1_GPIO_Port GPIOC
+#define RF_CTRL2_Pin GPIO_PIN_4
+#define RF_CTRL2_GPIO_Port GPIOA
+/* USER CODE END Private defines */
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
